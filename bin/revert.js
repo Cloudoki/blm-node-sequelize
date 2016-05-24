0f6fcc750de0f71c95b0e50d953b655696390d6c
efmr <edgar.marques.ribeiro@gmail.com>
2016-05-24 21:07:51 +0100

    lint

--- bin/revert.js
+++ bin/revert.js
@@ -1,37 +1,42 @@
 #!/usr/bin/env node
+
 'use strict';
+
 const debug = require('debug')('revert');
+
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'development';
 }
+
 const Umzug = require('umzug');
 const sequelize = require('../src/sequelize');
 const config = require('config');
 const sqlConfig = config.get('blm.sequelize');
 sqlConfig.logging = debug;
 debug('sqlConfig', sqlConfig);
+
 sequelize(sqlConfig)
-.then(function(db){
+.tap(db => db.sequelize.authenticate())
+.then(db => {
+  debug('db', Object.keys(db));
 
-    debug('db', Object.keys(db));
-    const umzugCfg = config.get('blm.umzug');
-    umzugCfg.storageOptions = {
-      sequelize: db.sequelize
-    };
-    umzugCfg.logging = debug;
+  const umzugCfg = config.get('blm.umzug');
+  umzugCfg.storageOptions = {
+    sequelize: db.sequelize
+  };
+  umzugCfg.logging = debug;
 
-    if (umzugCfg.migrations && umzugCfg.migrations.path) {
-      umzugCfg.migrations.params = [db.sequelize.getQueryInterface(),
-        db.Sequelize, db];
-    } else {
-      throw new Error('missing umzug migrations.path');
-    }
-    debug('umzugCfg', umzugCfg);
-    const umzug = new Umzug(umzugCfg);
-    return db.sequelize.authenticate().then(() => umzug.down());
-})
-.then(migration => {
-    debug('done', migration.file);
-    process.exit(1);
+  if (umzugCfg.migrations && umzugCfg.migrations.path) {
+    umzugCfg.migrations.params = [
+      db.sequelize.getQueryInterface(), db.Sequelize, db
+    ];
+  } else {
+    throw new Error('missing umzug migrations.path');
+  }
+  debug('umzugCfg', umzugCfg);
+  const umzug = new Umzug(umzugCfg);
+  return umzug.down();
+}).then(migration => {
+  debug('done', migration.file);
+  process.exit();
 });
-
